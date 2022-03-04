619b89134e4f1ed04a865c9f0f8481c6c60eeac6
miketillman95 <matillman95@gmail.com>
2022-03-03 20:59:29 -0700

    budget context complete

--- src/contexts/BudgetContexts.js
+++ src/contexts/BudgetContexts.js
@@ -1,5 +1,6 @@
-import { useContext } from "react"
+import React, { useState, useContext } from "react"
 import {v4 as uuidv4} from 'uuid'
+
 // context api
 const BudgetsContext = React.createContext()
 
@@ -25,30 +26,50 @@ export const BudgetsProvider = ({children}) => {
     const [budgets, setBudgets]= useState([])
        const [expenses, setExpenses]= useState([]) 
 
+// crud commands for updating budget and expenses 
+
 function getBudgetExpenses(budgetId){
-    return expenses.filter(expense => addExepense.budgetId ===budgetId)
+    return expenses.filter(expense => expense.budgetId ===budgetId)
 }
-function addExepense(){}
+function addExpense(description, amount, budgetId){
+    setExpenses(prevExpenses => {
+        return [...prevExpenses, {id: uuidv4(), description, amount, budgetId}]
+})
+}
+
 // making new budget by copying previous budget and adding new values
 function addBudget({name, max}){
     setBudgets(prevBudgets => {
+        if(prevBudgets.find(budget => budget.name ===name)){
+            return prevBudgets
+        }
         return [...prevBudgets, {id: uuidv4(), name, max}]
     })
 }
-function deleteBudget(){}
-function deleteExepenses(){}
+function deleteBudget({id}){
+    setBudgets(prevBudgets => {
+        return prevBudgets.filter(budget => budget.id !==id)
+    })
+}
+function deleteExpenses({id}){
+    setExpenses(prevExpenses => {
+        return prevExpenses.filter(expense => expense.id !==id)
+    })
+}
 
 return(
     <BudgetsContext.Provider value={{
         budgets,
         expenses,
         getBudgetExpenses,
-        addExepense,
+        addExpense,
         addBudget,
         deleteBudget,
-        deleteExepenses
+        deleteExpenses
 
     }}>{children}</BudgetsContext.Provider>
 )
 
 }
+
+export default BudgetsProvider
