dcfb1bbaba25f408f71a39f44c6323521fc30722
miketillman95 <matillman95@gmail.com>
2022-03-04 12:36:53 -0700

    api remove

--- src/api/Index.js
+++
@@ -1,8 +0,0 @@
-const server = require('./server')
-
-const PORT = 3001
-
-server.listen(PORT, () => {
-    console.log(`\n=== Server listening on port ${PORT} ===\n`)
-})
-// get port to run while FE runs
